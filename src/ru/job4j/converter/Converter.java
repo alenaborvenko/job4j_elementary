dc954961e25d74959f4ceb85f42ca2f3379ed3f1
Borvenko Alena <alenaborvenko@gmail.com>
2021-04-11 13:58:20 +0700

    0. Что такое тестирование [#515279]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -10,7 +10,15 @@ public class Converter {
     }
 
     public static void main(String[] args) {
+        float expect = 2f;
+        // сравнение вещественных чисел с точностью до второго знака, так как имеет смысл сравнивать только до копеек
+        boolean test = Math.abs(Converter.rubleToEuro(140) - expect) <= 1e-2;
         System.out.printf("140 rubles are %.2f euro\n", Converter.rubleToEuro(140));
+        System.out.println("rubleToEuro test = " + test);
+
+        expect = 2.33f;
+        test = Math.abs(Converter.rubleToDollar(140) - expect) <= 1e-2;
         System.out.printf("140 rubles are %.2f dollar\n", Converter.rubleToDollar(140));
+        System.out.println("rubleToDollar test = " + test);
     }
 }
