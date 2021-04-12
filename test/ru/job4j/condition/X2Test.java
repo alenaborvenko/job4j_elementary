a890c9e281b1792f9b5744c4814da1d06d8aedd7
Borvenko Alena <alenaborvenko@gmail.com>
2021-04-12 17:16:50 +0700

    1.2. Входные данные в тесте [#515285]

--- test/ru/job4j/condition/X2Test.java
+++ test/ru/job4j/condition/X2Test.java
@@ -8,16 +8,67 @@ import static org.junit.Assert.*;
 public class X2Test {
 
     @Test
-    public void whenCalcTest() {
-        int[] a = {1, 0, 1, 1};
-        int[] b = {1, 1, 1, 1};
-        int[] c = {1, 1, 0, 1};
-        int[] x = {1, 1, 1, 0};
-        int[] expected = {3, 2, 2, 1};
-        int[] resultCalcMethod = new int[x.length];
-        for (int i = 0; i < x.length; i++) {
-            resultCalcMethod[i] = X2.calc(a[i], b[i], c[i], x[i]);
-        }
-        Assert.assertArrayEquals(expected, resultCalcMethod);
+    public void whenA10B0C0X2Then40() {
+        /* Входные параметры. Их будет 4 штуки */
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        /* ожидаемое значение. Это всегда одна переменная. */
+        int expected = 40;
+        /* вызов метода, который хотим проверить. */
+        /* Результат вычисления. Это всегда одна переменная. */
+
+        int rsl = X2.calc(a, b, c, x);
+        /* сравнение полученного значения с ожидаемым */
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C1X1Rsl3() {
+        // Входные параметры
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C1X1Rsl2() {
+        // Входные параметры
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C0X1Rsl2() {
+        // Входные параметры
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C1X0Rsl1() {
+        // Входные параметры
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
     }
 }
