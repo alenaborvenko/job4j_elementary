7204ab4b2792180a412459f5efc1d533ed8d2bd6
Borvenko Alena <alenaborvenko@gmail.com>
2021-04-13 20:22:09 +0700

    5.5. Простое число [#515308]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,7 +4,8 @@ public class CheckPrimeNumber {
 
     public static boolean check(int number) {
         // число 1 не является ни простым числом, ни составным. У него всего 1 делитель. И для числа 1 надо вернуть false
-        boolean prime = number != 1;
+        // простые числа - только НАТУРАЛЬНЫЕ числа по определению
+        boolean prime = number > 1;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false;
